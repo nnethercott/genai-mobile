7d309e1248ba5a63a8097f1deac46d363dee4e0d
nnethercott <nathaniel@deepomatic.com>
2025-03-10 22:20:06 +0100

    Add download_model script

---
+++ download_models.sh
@@ -0,0 +1,1 @@
+[ -f assets/models/qwen_small] || wget https://huggingface.co/Qwen/Qwen2.5-0.5B-Instruct-GGUF/resolve/main/qwen2.5-0.5b-instruct-q2_k.gguf?download=true -O assets/models/qwen_small.gguf
